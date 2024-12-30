e05b07a23ede138b0fb744c3a4e56717988356ca
Shinei Nouzen <157171073+Shineii86@users.noreply.github.com>
2024-12-30 06:03:09 +0530

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,34 @@
+// Navigation bar and tabContent/links
+function openPage(pageName,elmnt,color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(pageName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+// HTML/CSS/JS Code Editor Using TextArea
+function compile() {
+
+	var html = document.getElementById("html");
+	var css = document.getElementById("css");
+	var js = document.getElementById("js");
+	var code = document.getElementById("code").contentWindow.document;
+	
+	 document.body.onclick = function(){
+	    code.open();
+		code.writeln(html.value+"<style>"+css.value+"</style>"+"<script>" + js.value + "</script>");
+		code.close();
+      };
+    };
+
+compile();
